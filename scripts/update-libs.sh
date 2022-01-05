036b92c7894e473be952d75e38e44fb9b99f85f8
Emerson Valentim <emevalentim0@gmail.com>
2022-01-05 12:32:23 -0300

    chore: update libs scripts

--- scripts/update-libs.sh
+++ scripts/update-libs.sh
@@ -6,7 +6,7 @@ cd services;
 for d in */ ; do
 	echo "Updating node_modules in $d";
     rm -rf $d/node_modules;
-    cd $d; yarn; cd ..;
+    cd $d; yarn; docker-compose build; cd ..;
 done
 
 cd ..;
