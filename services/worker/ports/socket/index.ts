e22d5ce7b6fbc71bb480747e46557ce588b84837
Emerson Valentim <emevalentim0@gmail.com>
2022-01-23 13:11:08 -0300

    fix: update socket host variables reference

--- services/worker/ports/socket/index.ts
+++ services/worker/ports/socket/index.ts
@@ -1,7 +1,7 @@
 import { Managers } from "evs-tools";
 
 Managers.SocketManager.add([
-  { host: `http://${process.env.SOCKET_HOST}:3000/`, instance: "socket-1" },
+  { host: `${process.env.SOCKET_HOST!}:${process.env.SOKCET_PORT!}`, instance: "socket-1" },
 ]);
 
 export default Managers.SocketManager;
